ffbded68dd209a06d3d89b96c80aa6da98d40e0b
unknown <wkevg12@gmail.com>
2020-06-07 18:39:13 -0400

    Updates

---
+++ precache-manifest.91938f2f78a79a52ba215f5dc9614d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4c84a782726626e1779074c156fb09",
+    "url": "/Finding_Recycling_Bin/index.html"
+  },
+  {
+    "revision": "60cd798579d2ba7ea515",
+    "url": "/Finding_Recycling_Bin/static/css/main.ed84b804.chunk.css"
+  },
+  {
+    "revision": "fbaaf39e7cd041ec61a5",
+    "url": "/Finding_Recycling_Bin/static/js/2.af8d66a1.chunk.js"
+  },
+  {
+    "revision": "76337fd691e4155561d496e490beeac9",
+    "url": "/Finding_Recycling_Bin/static/js/2.af8d66a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cd798579d2ba7ea515",
+    "url": "/Finding_Recycling_Bin/static/js/main.3e3a3be2.chunk.js"
+  },
+  {
+    "revision": "7fa826b4c1e4ffb8b5a3",
+    "url": "/Finding_Recycling_Bin/static/js/runtime-main.5921dd22.js"
+  }
+]);
